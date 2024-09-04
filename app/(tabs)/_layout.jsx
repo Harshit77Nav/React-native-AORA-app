3787b041daa08887522ea4e19548d7957b77eabf
Harshit77Nav <harshit77navneet@gmail.com>
2024-09-04 17:08:44 +0530

    updated folder structure

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,31 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { Tabs, Redirect } from 'expo-router';
+
+const TabIcon = ({icon, color, name, focused}) => {
+    return (
+        <View>
+            <Image source={""} />
+        </View>
+    )
+}
+
+const TabsLayout = () => {
+    return (
+        <>
+            <Tabs>
+                <Tabs.Screen 
+                name="home"
+                options={{
+                    title: 'Home',
+                    headerShown: false,
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabIcon icon={""}/>
+                    )
+                }}/>
+            </Tabs>
+        </>
+    )
+}
+
+export default TabsLayout
--- app/index.jsx
+++ app/index.jsx
@@ -8,7 +8,7 @@ const App = () => {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-red-800 text-3xl" >Aora!</Text>
       <StatusBar style='auto'/>
-      <Link href={'/profile'}>Go to profile</Link>
+      <Link href={'/home'}>Go to Home</Link>
     </View>
   )
 }
