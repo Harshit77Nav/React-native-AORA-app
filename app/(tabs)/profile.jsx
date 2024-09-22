8cd82b15ca6fd4927b053c6453000bc6fcaa67d0
Harshit77Nav <harshit77navneet@gmail.com>
2024-09-22 22:34:12 +0530

    added dummy data

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,11 +1,36 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+    <Text className="text-white">Create</Text>
+  </SafeAreaView>
   )
 }
 
